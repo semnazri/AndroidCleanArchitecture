8317666ac5f30b7c1a49b898923244ef3d64ba1d
SemNazri_Aja <syamsul.bahri@j-express.id>
2023-01-16 17:49:25 +0700

    Initial commit and Migrate from groovy to DSL

---
+++ app/src/main/java/com/semnazri/cleanArchitecture/build/Deps.kt
@@ -0,0 +1,5 @@
+package com.semnazri.cleanArchitecture.build
+
+object Deps {
+    const val ktxCore = "androidx.core:core-ktx:${Version.ktxVer}"
+}
---
+++ app/src/main/java/com/semnazri/cleanArchitecture/build/Version.kt
@@ -0,0 +1,5 @@
+package com.semnazri.cleanArchitecture.build
+
+object Version {
+    const val ktxVer = "1.9.0"
+}
